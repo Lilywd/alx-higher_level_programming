0a667b1ff16ecf0945614d599f4079be2e0cc75f
Lilywd <ndungulilianwanjiku@gmail.com>
2022-09-05 22:07:46 +0300

    warmup.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* arguments in js */
+const myArgs = process.argv.length;
+if (myArgs < 3) {
+  console.log('No argument');
+} else if (myArgs === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
